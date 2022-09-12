0f641069b5f883cf4a79f134a6e3c0ee3e033648
Lillie Schachter <lillieschachter@gmail.com>
2022-09-12 12:16:58 -0500

    clean up stocks instructions

--- stocks.py
+++ stocks.py
@@ -7,9 +7,11 @@ def stock_purchases():
     msft = 200
 
     # Given the prices above and a client's investment budget, how much stock can they buy?
-    # 1.1 TODO: Ask the client's name and save it into a variable
+    # 1.1 TODO: Ask the client's name (use the string: "What is your name? ") and save it into a variable
 
-    # 1.2 TODO: Ask the client how many dollars they would like to invest and save it into a variable
+    # 1.2 TODO: Ask the client how many dollars they would like to invest (use the string: "How much would you like to invest? $")
+    # and save it into a variable
+    # NOTE: When you use the `input` function to get user input, what do numbers get saved as?
 
     # 1.3 TODO: Uncomment the line below to ask the client which stock they're interested in.
     # NOTE: Take a look at how this input string prints out
@@ -18,8 +20,8 @@ def stock_purchases():
     # Now that you have all of the client info, you can check how much they can purchase of the stock
     # they're interested in.
 
-    # 1.4 TODO: Use `if/elif/else` conditional logic to determine how much stock the client can buy:
-    # NOTE: When use use the `input` function to get user input, what do numbers get saved as?
+    # 1.4 TODO: Use `if/elif/else` conditional logic to determine how much stock the client can buy,
+    # and save it in a variable
 
     # 1.5 TODO: Once you've calculated the number of stocks that can be purchased,
     # Use an f-string to print the result for the client, ala:
